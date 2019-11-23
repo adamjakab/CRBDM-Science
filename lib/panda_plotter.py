eba4779bec617f0d5a7fecbb0dff7e63e0c3e7b6
adamjakab <jakabadambalazs@gmail.com>
2019-11-23 23:54:11 +0100

    graph configuration

--- lib/panda_plotter.py
+++ lib/panda_plotter.py
@@ -5,8 +5,10 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-from seaborn import xkcd_rgb, crayons
+from deepmerge import conservative_merger, always_merger
 from pandas.plotting import register_matplotlib_converters
+import pandas as pd
+from seaborn import xkcd_rgb
 
 
 class PandaPlotter:
@@ -23,55 +25,94 @@ class PandaPlotter:
 
         # Other
         register_matplotlib_converters()
-        sns.set_style("whitegrid")
 
+    @staticmethod
+    def _get_default_plot_config():
+        cfg = {
+            "title": "Untitled Plot",
+            "plots": [],
+            "style": {
+                "width": 10,
+                "height": 5,
+                "dpi": 96,
+                "default_style": "whitegrid",
+                "palette_color": "baby shit green",
+                "line_width": 1,
+                "line_style": "solid",
+                "fill_alpha": 0.2
+            }
+        }
+        return cfg
 
-    def plot(self, plotconfig, df, show=True, save=False):
-        fig = plt.figure(num=None, figsize=(10, 5), dpi=96, facecolor='w', edgecolor='k')
+    @staticmethod
+    def _get_default_subplot_config():
+        cfg = {
+            "data": pd.DataFrame([]),
+            "title": "Untitled Plot",
+            "kind": "line",
+            "x_column": "x",
+            "y_column": "y",
+            "x_label": "X",
+            "y_label": "Y",
+            "x_major_ticks_freq": 10,
+            "x_minor_ticks_freq": 1,
+            "y_major_ticks_freq": 10,
+            "y_minor_ticks_freq": 1,
+        }
+        return cfg
+
+    def plot(self, cfg, show=True, save=False):
+        # Merge the default configuration with the selected configuration section
+        pconf = always_merger.merge(self._get_default_plot_config(), cfg)
+        for i in range(0, len(pconf["plots"])):
+            pconf["plots"][i] = always_merger.merge(self._get_default_subplot_config(), pconf["plots"][i])
+        print(pconf)
+
+        # Styles and Colors
+        # Use colors from : (from .colors import xkcd_rgb, crayons)
+        # sns.set_style(pconf["style"]["default_style"])
+        palette_color = xkcd_rgb[pconf["style"]["palette_color"]]
+        palette = sns.dark_palette(palette_color, n_colors=7)
+
+        fig = plt.figure(num=None, figsize=(pconf["style"]["width"], pconf["style"]["height"]),
+                         dpi=pconf["style"]["dpi"], facecolor='w', edgecolor='k')
+
+        # For now only single subplots are supported
         ax = fig.add_subplot(1, 1, 1)
 
-        # Colors (from .colors import xkcd_rgb, crayons)
-        palette_color = xkcd_rgb["baby shit green"]
-        if "palette_color" in plotconfig:
-            palette_color = xkcd_rgb[plotconfig["palette_color"]]
-        palette = sns.dark_palette(palette_color)
+        # Data for subplot
+        subpconf = pconf["plots"][0]
+        df = subpconf["data"]
 
         # X
-        x_column = plotconfig["x_column"]
+        x_column = subpconf["x_column"]
         x = df[x_column]
 
-        # Allow for multiple columns passed as a list
-        if isinstance(plotconfig["y_column"], list):
-            y_columns = plotconfig["y_column"]
+        # Y (Allow for multiple columns passed as a list)
+        if isinstance(subpconf["y_column"], list):
+            y_columns = subpconf["y_column"]
         else:
-            y_columns = [plotconfig["y_column"]]
+            y_columns = [subpconf["y_column"]]
 
         # Plot
-        for y_column in y_columns:
+        for i in range(0, len(y_columns)):
+            y_column = y_columns[i]
             y = df[y_column]
-            ax.plot(x, y, color=palette[3], lw=1, linestyle='solid')
-            ax.fill_between(x, 0, y, alpha=.2, facecolor=palette[3])
+            color = palette[i+2]
+            ax.plot(x, y, color=color, lw=pconf["style"]["line_width"], linestyle=pconf["style"]["line_style"])
+            ax.fill_between(x, 0, y, alpha=pconf["style"]["fill_alpha"], facecolor=color)
 
         # X ticks
-        x_major_ticks_freq = 100
-        x_minor_ticks_freq = 25
-        if "x_major_ticks_freq" in plotconfig:
-            x_major_ticks_freq = plotconfig["x_major_ticks_freq"]
-        if "x_minor_ticks_freq" in plotconfig:
-            x_minor_ticks_freq = plotconfig["x_minor_ticks_freq"]
-
+        x_major_ticks_freq = subpconf["x_major_ticks_freq"]
+        x_minor_ticks_freq = subpconf["x_minor_ticks_freq"]
         x_major_ticks = x[0::x_major_ticks_freq]
         x_minor_ticks = x[0::x_minor_ticks_freq]
         ax.set_xticks(x_major_ticks)
         ax.set_xticks(x_minor_ticks, minor=True)
 
         # Y ticks
-        y_major_ticks_freq = 100
-        y_minor_ticks_freq = 25
-        if "y_major_ticks_freq" in plotconfig:
-            y_major_ticks_freq = plotconfig["y_major_ticks_freq"]
-        if "y_minor_ticks_freq" in plotconfig:
-            y_minor_ticks_freq = plotconfig["y_minor_ticks_freq"]
+        y_major_ticks_freq = subpconf["y_major_ticks_freq"]
+        y_minor_ticks_freq = subpconf["y_minor_ticks_freq"]
 
         y_column = y_columns[0]
         y = df[y_column]
@@ -82,15 +123,25 @@ class PandaPlotter:
         ax.set_yticks(y_major_ticks)
         ax.set_yticks(y_minor_ticks, minor=True)
 
-        # And a corresponding grid
-        ax.grid(which='both')
-
-        # Or if you want different settings for the grids:
-        ax.grid(which='minor', alpha=0.4)
+        # Grid & Ticks
         ax.grid(which='major', alpha=0.8)
+        ax.grid(which='minor', alpha=0.4)
+        ax.tick_params(which='major', direction='inout', pad=5, length=3, width=1, color="k")
+        ax.tick_params(which='minor', length=0, width=0)
 
-
+        # Plot Spacing
         plt.subplots_adjust(top=0.93, bottom=0.2, left=0.07, right=0.97, hspace=0.2, wspace=0.2)
+
+
+
+        if show:
+            plt.show()
+
+    def plot_(self, cfg, df, show=True, save=False):
+
+
+
+
 
 
         ax.set_title(plotconfig["plot_title"], fontsize=12)
@@ -119,4 +170,3 @@ class PandaPlotter:
         extension = "png"
         plot_file_path = "{0}/images/{1}.{2}".format(self.__base_dir__, clean_name, extension)
         return plot_file_path
-
--- p1.py
+++ p1.py
@@ -5,7 +5,6 @@
 #  Email: adaja at itu dot dk
 #
 #
-import pandas as pd
 from lib.cached_data_loader import CachedDataLoader
 from lib.panda_plotter import PandaPlotter
 
@@ -13,26 +12,36 @@ from lib.panda_plotter import PandaPlotter
 cdl = CachedDataLoader()
 pp = PandaPlotter()
 
-sql = 'SELECT v.ts AS TS, v.mqtt_count AS BATCH_COUNT FROM v__wcb__h__record_counts AS v'
+sql = 'SELECT v.ts AS TS, ' \
+      'v.mqtt_count AS BATCH_COUNT, ' \
+      'v.per_minute_count AS PER_MINUTE_COUNT ' \
+      'FROM v__wcb__h__record_counts AS v'
 df = cdl.get_dataframe(sql, reindex=True)
 
 # Filter a time period by mask
 mask = (df['TS'] >= '2019-10-21 00:00:00') & (df['TS'] < '2019-10-26 00:00:00')
-mdf = df.loc[mask]
-# print(mdf)
+mdf_1 = df.loc[mask]
+# print(mdf_1)
+
 
 plotconfig = {
-    "plot_title": "Major Downtime 23-24 October",
-    "kind": "line",
-    "x_column": "TS",
-    "y_column": "BATCH_COUNT",
-    "x_label": "Timestamp",
-    "y_label": "Mqtt batch count per hour",
-    "x_major_ticks_freq": 24,
-    "x_minor_ticks_freq": 6,
-    "y_major_ticks_freq": 2,
-    "y_minor_ticks_freq": 1,
-    "palette_color": "purple"
+    "title": "Jack-1",
+    "plots": [
+        {
+            "data": mdf_1,
+            "title": "Major Downtime 23-24 October",
+            "x_column": "TS",
+            "y_column": ["BATCH_COUNT"],
+            "x_label": "Timestamp",
+            "y_label": "Mqtt batch count per hour",
+            "x_major_ticks_freq": 24,
+            "x_minor_ticks_freq": 6,
+            "y_major_ticks_freq": 2,
+            "y_minor_ticks_freq": 1,
+        }
+    ],
+    "style": {
+        "palette_color": "purple"
+    }
 }
-pp.plot(plotconfig, mdf, save=True)
-
+pp.plot(plotconfig, save=True)
